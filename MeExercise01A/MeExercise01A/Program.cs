16474d57ddfd52c6b9d00f272102ba47ad85147c
marstab4 <47764604+marstab4@users.noreply.github.com>
2019-04-04 12:41:32 +1100

    some change

---
+++ MeExercise01A/MeExercise01A/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeExercise01A
+{
+    class Program
+    {
+        public int FirstName; //style 1 Pascalcase
+        public int firstName; //style 2 camelCase
+        public int first_name; //style 3- snake_case  
+
+
+
+        static void Main(string[] args)
+        {
+            //quick change
+
+
+        }
+    }
+}
